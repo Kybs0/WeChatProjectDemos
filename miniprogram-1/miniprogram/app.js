31d90439d61ecd7f393ac546f4803b4d28346dcb
余冬 <yudong@cvte.com>
2019-12-28 21:44:19 +0800

    第一个小程序

---
+++ miniprogram-1/miniprogram/app.js
@@ -0,0 +1,19 @@
+//app.js
+App({
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+  },
+  getUserInfo: function (cb) {
+
+  },
+  onShow(options) {
+    // Do something when show.
+  },
+  onHide() {
+    // Do something when hide.
+  },
+  onError(msg) {
+    console.log(msg)
+  },
+  globalData: 'aaaa'
+})
